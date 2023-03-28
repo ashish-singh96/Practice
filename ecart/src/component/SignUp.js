96e09036a087f173deaaa1549d093de6f7546125
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-03-28 12:30:16 +0530

    second way

--- ecart/src/component/SignUp.js
+++ ecart/src/component/SignUp.js
@@ -1,58 +1,44 @@
-import React,{useState}from 'react'
+import React,{useState} from 'react'
 
 const SignUp = () => {
-  const [firstname, setFirstName] = useState('');
-  const[lastname,setLastName]=useState('');
-  const[emailaddress,setEmailAddress]=useState('');
-  const[password,setPassword]=useState('');
+  const[logInDetails,setOriginalDetail]=useState({
+    firstname:'',
+    lastname:'',
+    emailaddress:'',
+  });
+  
 
-  const handleFirstNameChange=(event)=>{
-    setFirstName(event.target.value);
-  }
-  const handleEmailAddress=(event)=>{
-    setEmailAddress(event.target.value);
-  }
-  const handleLastNameChange=(event)=>{
-    setLastName(event.target.value);
-  }
-
-  const handlePassword=(event)=>{
-    setPassword(event.target.value);
+  const handleChange=(event)=>{
+    const{name,value}=event.target;
+    setOriginalDetail({...logInDetails,[name]:value})
   }
 
   const handleSubmit=(event)=>{
     event.preventDefault();
-    console.log(firstname);
-    console.log(lastname);
-    console.log(emailaddress);
-    console.log(password);
+    console.log(logInDetails);
   }
 
   return (
     <div className='signUp'>
-       <form >
-       <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">First Name</label>
-    <input type="email" name="firstname" onChange={(event=>handleFirstNameChange(event))}  className="form-control" placeholder='Enter First Name'/>
+      <form>
+      <div class="mb-3">
+    <label for="exampleInputEmail1" className="form-label">FirstName</label>
+    <input type="email" name="firstname" onChange={(event=>handleChange(event))} className="form-control"  placeholder='Enter First Name'/>
+    
   </div>
 
   <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">LastName</label>
-    <input type="email" name="setlastname" onChange={(event=>handleLastNameChange(event))} className="form-control" placeholder='Enter Last Name'/>
+    <label for="exampleInputPassword1" className="form-label">LastName</label>
+    <input type="text"  name="lastname" onChange={(event=>handleChange(event))} className="form-control" placeholder='Enter LastName'/>
   </div>
 
   <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" name="emailaddress" onChange={(event=>handleEmailAddress(event))} className="form-control" placeholder='Enter Email address'/>
+    <label for="exampleInputPassword1" className="form-label">Email Address</label>
+    <input type="text"  name="emailaddress" onChange={(event=>handleChange(event))} className="form-control" placeholder='Enter LastName'/>
   </div>
 
-  <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password"name="password" onChange={(event=>handlePassword(event))} className="form-control" placeholder='Enter password'/>
-  </div>
- 
-  <button onClick={(e)=>handleSubmit(e)} className="btn btn-primary">Submit</button>
-</form>
+  <button onClick={(event)=>handleSubmit(event)} class="btn btn-primary">Submit</button>
+      </form>
     </div>
   )
 }
